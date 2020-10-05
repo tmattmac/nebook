80a40d925b9d935cd836efe97119e5c5e309b362
Trey McDeane <tmcdeane@gmail.com>
2020-10-05 13:02:27 -0400

    added config for reader blueprint

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from models import db, connect_db
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///feedback'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = 'somesecretkey'
 
 connect_db(app)
+
+from reader import reader
+app.register_blueprint(reader, url_prefix='/reader')
 
 @app.route('/')
 def index():
---
+++ reader.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+from os.path import join
+from app import app
+
+reader_path = join(app.root_path, 'reader/reader')
+reader = Blueprint('reader', __name__, static_folder=reader_path)
+
+@reader.route('/')
+def show_reader():
+    # TODO: Pass in google drive book handle
+    return render_template('reader.html')
