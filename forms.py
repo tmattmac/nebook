a7e94d31768b5d7dfdfeebe1564dfef8967f42de
Trey McDeane <tmcdeane@gmail.com>
2020-10-11 16:35:33 -0400

    added auth forms

--- forms.py
+++ forms.py
@@ -1,2 +1,32 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
+from wtforms.validators import Length, DataRequired, Email
+
+class UserLoginForm(FlaskForm):
+
+    username = StringField(
+        'Username',
+        validators=[DataRequired()]
+    )
+
+    password=PasswordField(
+        'Password',
+        validators=[DataRequired(), Length(min=8)]
+    )
+
+class UserRegisterForm(FlaskForm):
+
+    username = StringField(
+        'Username',
+        validators=[DataRequired()]
+    )
+
+    email = StringField(
+        'Email address',
+        validators=[DataRequired(), Email()]
+    )
+
+    password=PasswordField(
+        'Password',
+        validators=[DataRequired(), Length(min=8)]
+    )
